26301ecdaf49769f233c817b14ab1f9872440362
Jin Pan <jinpan@mit.edu>
2013-12-10 12:08:29 -0500

    included omega test runs

--- src/run_experiments.sh
+++ src/run_experiments.sh
@@ -1,33 +1,15 @@
 #! /usr/bin/zsh
 
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
-echo 0 | python experiments.py > /dev/null &
-echo 1 | python experiments.py > /dev/null &
-echo 2 | python experiments.py > /dev/null &
+echo 0 | pypy experiments.py > /dev/null &
+echo 1 | pypy experiments.py > /dev/null &
+echo 2 | pypy experiments.py > /dev/null &
+echo 3 | pypy experiments.py > /dev/null &
+echo 0 | pypy experiments.py > /dev/null &
+echo 1 | pypy experiments.py > /dev/null &
+echo 2 | pypy experiments.py > /dev/null &
+echo 3 | pypy experiments.py > /dev/null &
+echo 0 | pypy experiments.py > /dev/null &
+echo 1 | pypy experiments.py > /dev/null &
+echo 2 | pypy experiments.py > /dev/null &
+echo 3 | pypy experiments.py > /dev/null &
 
